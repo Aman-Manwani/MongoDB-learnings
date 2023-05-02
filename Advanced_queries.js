a5a0a96ee468afd9abba2435b77d284b34de3365
Aman Manwani <aman04.manwani@gmail.com>
2023-05-03 02:18:32 +0530

    added operational queries

--- Advanced_queries.js
+++ Advanced_queries.js
@@ -86,13 +86,32 @@ https://www.youtube.com/watch?v=wPL2i8KHKTI&list=PLA3GkZPtsafZydhN4nP0h7hw7PQuLs
 // 1 for ascending -1 for descending
 db.collection.find().sort({age:1});
 
+//  inc - increment
+//  min - kam karna h
+//  max - jyada karna h 
+//  mul - to multiply
+//  unset - to remove the specified field from document or documents
+// set - to set/add a new specified field in a document
+// rename - to rename the field value
+// upsert - update if present otherwise make a document of specified details
 
+db.collection.updateMany({},{$inc:{age:2}});
+// increase all the documents age by 2
 
+// if age is 23 and we have to make it 50, then we use max and vice versa with min
+db.students.updateOne({name:"Sita"},{$min:{age:23}})
 
+db.students.updateOne({name:"Sita"},{$mul:{age:2}})
+// multiply the age of sita's document by 2 
 
+db.students.updateOne({name:"Sita"},{$unset:{age:2458}})
+//2458 ki jagah kuch bhi likh skte h because these field will be removed 
 
+db.students.updateMany({},{$rename:{age:"students_age"}})
+// changes name of field age to students_age
 
-
+db.students.updateOne({name:"Golu"},{$set:{age:100}},{upsert:true});
+// if present then change the age to 100 otherwise make the document with two fields name and age
 
 
 
